362c100d2d08607f591a09029473519a7bbd3651
Isaac <finken.isaac@gmail.com>
2020-08-12 11:57:12 -0500

    Successfully generated tokens array in signinstate.js

--- public/js/signinstate.js
+++ public/js/signinstate.js
@@ -2,22 +2,20 @@ document.addEventListener('DOMContentLoaded', async() => {
     const loginLink = document.querySelector('#loginLink');
     const signUpLink = document.querySelector('#signUpLink');
     const logoutLink = document.querySelector('#logoutLink');
-    // const res = await fetch(`api/users`)
-    console.log(document.cookie);
-    let tokens = [];
-    const cookieArr = document.cookie.split(';');
-    const tokenArr = cookieArr.filter(cookie => cookie.slice(0, 6) === 'token=')
-    console.log(tokenArr);
-    // if (document.cookie.split(';').some((item) => item.trim().startsWith('token='))) { tokens.push(item) }
+
+    const tokens = document.cookie.split(';').filter(cookie => cookie.slice(0, 6) === 'token=').map(token => token.slice(6));
     console.log(tokens);
+    // tokenArr.forEach()
+    // console.log(tokens);
+
     //how to find if user is logged in? Set as boolean
-    if (userSignedIn) {
-        loginLink.classList.add('hidden');
-        signUpLink.classList.add('hidden');
-        logoutLink.classList.remove('hidden');
-    } else {
-        loginLink.classList.remove('hidden');
-        signUpLink.classList.remove('hidden');
-        logoutLink.classList.add('hidden');
-    }
+    // if (userSignedIn) {
+    //     loginLink.classList.add('hidden');
+    //     signUpLink.classList.add('hidden');
+    //     logoutLink.classList.remove('hidden');
+    // } else {
+    //     loginLink.classList.remove('hidden');
+    //     signUpLink.classList.remove('hidden');
+    //     logoutLink.classList.add('hidden');
+    // }
 })
