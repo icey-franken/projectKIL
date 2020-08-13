d1ece623d01a569b43c4f0ac3ab94d7fd2039399
Isaac <finken.isaac@gmail.com>
2020-08-12 20:15:36 -0500

    WE FIXED ITgit statusgit status - We had null as a string, so it wasn't caught by our validations in users.js.

--- utils.js
+++ utils.js
@@ -1,11 +1,5 @@
-const { validationResult } = require('express-validator');
-
 const asyncHandler = (handler) => (req, res, next) => {
     handler(req, res, next).catch(next);
 }
 
-// const handleValidationErrors //add this bullshit
-
-
-
-module.exports = { asyncHandler, }; //add handleValidationErrors
+module.exports = { asyncHandler };
