958b5e1dfbdc9bf524b55fbfe631be387041d3df
Krisna Sarith <krisnasarith@gmail.com>
2020-08-13 00:53:30 -0700

    comment deletion completed for now

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { asyncHandler} = require('../../utils');
+const { asyncHandler } = require('../../utils');
 const { Comment, User, Project } = require('../../db/models');
 const { check } = require('express-validator');
 
@@ -39,20 +39,7 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     res.json({ comment });
 }))
 
-router.delete('/:id(\\d+)', asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
-    const comment = await Comment.findAll({
-        where: {
-            id
-        }
-    });
-    console.log(comment);
-    if (comment) {
-        await comment[0].destroy();
-        res.json({ message: `Deleted comment with id of ${req.params.id}.` });
-    }
-    else next();
-}))
+
 
 router.get('/project/:id(\\d+)', asyncHandler(async (req, res) => {
     const { id } = req.params;
@@ -80,31 +67,19 @@ router.get('/user/:id(\\d+)', asyncHandler(async (req, res) => {
 // /projects/:projectId/:projectName -- editing an existing project. Unsure about this - should projectId be aliased to a projectName instead, or should we provide both for clarity?
 
 //this route simply gets the form in which users can edit their project.
-router.get('/edit/:id(\\d+)', asyncHandler(async (req, res) => {
-    const commentId = parstInt(req.params.id, 10);
-    const comment = await Comment.findByPk(commentId, {
-        include: [], //eager loading
+router.patch('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const comment = await Comment.findAll({
+        where: {
+            id
+        }
     });
-    res.json({ comment });
-}))
-
-//When users make edits and save them they are 'submitting the project-edit-form' - it is a post request to the same route.
-
-//validations for editing a project -- these should be similar to the validations we implement on our database. We do them here as well so make sure the stuff we are sending to our database are properly submitted. Soon-Mi called this "sanitizing our database inputs"
-// const checkProjectEdits = [
-// 	check('bullshit')
-// 	.yadda yadda
-// ];
-
-//I think since this is a post request we should have some validation things in here - using the check and validationHandler shit from the express-validator package or whatever.
-router.post('/edit/:projectId(\\d+)', asyncHandler(async (req, res) => { //add checkProjectEdits and handleValidationErrors middleware
-    //destructure body of requst to get the shit the users want to edit about the project
-    const { destructuredShit } = req.body
-    const project = await Project.update({ destructuredShit })
-    //we'll have an error handler or something in case the project isn't updated correctly.
-    //If project IS updated correctly, then we want to render the edit form once again but with the new project stuff.
-    //We probably also want to save the previous version of the project somewhere in our database for history purposes, but maybe we worry about that later on.
-    res.render('project-edit-form', { project });
+    console.log(comment);
+    if (comment) {
+        await comment[0].update({ comment: req.body.updatedComment }, { where: id });
+        res.json({ message: `Patched comment with id of ${req.params.id}.` });
+    }
+    else next();
 }))
 
 
@@ -112,11 +87,19 @@ router.post('/edit/:projectId(\\d+)', asyncHandler(async (req, res) => { //add c
 // /projects/edit/deleted -- it is in project-readme that this is the route after project deletion. Maybe this is a confirmation page that project has been destroyed? If so, we can use the same page for each deletion (i.e. no specific project id or other identifying info). This will be the page we show upon successful Project.destroy(where: {id: projectId}) operation.
 
 //we should have a button on the project edit page that allows a user to delete. If they click delete they should be sent to this route, and then redirected to a /projects/edit/deleted page to confirm successful deletion.
-router.post('/edit/:projectId(\\d+)/delete', asyncHandler(async (req, res) => {
-    const projectId = parseInt(req.params.projectId, 10);
-    const deletedProjectName = await Project.findByPk(projectId).name;
-    await Project.destroy(projectId);
-    res.render('project-deleted-page', { deletedProjectName }) //we pass in the name to the project deleted page so we can say something like "Your Destructable 'How to build a backyard nuke' has been successfully destructed."
+router.delete('/:id(\\d+)', asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const comment = await Comment.findAll({
+        where: {
+            id
+        }
+    });
+    console.log(comment);
+    if (comment) {
+        await comment[0].destroy();
+        res.json({ message: `Deleted comment with id of ${req.params.id}.` });
+    }
+    else next();
 }))
 
 
