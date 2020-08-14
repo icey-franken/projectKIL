9a65f8bd85ed003ff41d3497eacc0b2511dc898b
Isaac <finken.isaac@gmail.com>
2020-08-13 19:51:22 -0500

    Cleaned up projects-new-form.js - can grab userId and associate with project before creation.

--- public/js/projects-new-form.js
+++ public/js/projects-new-form.js
@@ -6,54 +6,34 @@ document.addEventListener('DOMContentLoaded', async() => {
         //first we check that user signed in
         const cookies = document.cookie;
 
-        let data;
-        try {
-            const res1 = await fetch('/api/users/signinstate', {
+        const res1 = await fetch('/api/users/signinstate', {
+            method: 'post',
+            body: JSON.stringify({ cookies }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data1 = await res1.json();
+        if (!res1.ok) {
+            const { message } = data1;
+            const errorsContainer = document.querySelector('#errors-container');
+            errorsContainer.innerHTML = message;
+            return;
+        }
+        const { userSignedIn, token } = data1;
+        //if they are, we get the userId and proceed with new project creation
+        if (userSignedIn) {
+            const formData = new FormData(createProjectForm);
+            const name = formData.get('name');
+            const res2 = await fetch('/api/users/getUserId', {
                 method: 'post',
-                body: JSON.stringify({ cookies }),
+                body: JSON.stringify({ token }),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
-            console.log('line18 res1', res1);
-            data = await res1.json();
-            if (!res1.ok) {
-                const { message } = data;
-                const errorsContainer = document.querySelector('#errors-container');
-                errorsContainer.innerHTML = message;
-                return;
-            }
-        } catch (e) { console.log(e) };
-        const { userSignedIn } = data;
-        const { token } = data;
-
-        console.log('line30res1', userSignedIn, token)
-            //if they are, we get the userId and proceed with new project creation
-        if (userSignedIn) {
-            //get user id from signinstate above???
-            const formData = new FormData(createProjectForm);
-            const name = formData.get('name')
-            let res2;
-            try {
-                res2 = await fetch('/api/users/getUserId', {
-                    method: 'post',
-                    body: JSON.stringify({ token }),
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
-            } catch (e) { console.log('line45', e) };
-            console.log('line46 res2', res2);
-            let userId2;
-            try {
-                let data = await res2.json();
-                console.log('line50', data)
-                let { userId } = data;
-                console.log('line52', userId);
-                userId2 = userId;
-            } catch (e) { console.log('line53', e); }
-            console.log('userid', userId2);
-            let userId = userId2;
+            const data2 = await res2.json();
+            const { userId } = data2;
             const body = { name, userId };
             const res3 = await fetch('/api/projects/new', {
                 method: 'post',
@@ -62,18 +42,17 @@ document.addEventListener('DOMContentLoaded', async() => {
                     'Content-Type': 'application/json'
                 }
             })
-            const data = await res3.json();
+            const data3 = await res3.json();
             if (!res3.ok) {
-                const { message } = data;
+                const { message } = data3;
                 const errorsContainer = document.querySelector('#errors-container');
                 errorsContainer.innerHTML = message;
                 return;
             }
-            const { project } = data;
-            console.log('line 47 in projects-new-form.js', project, project.userId, project.name);
+            const { project } = data3;
             window.location.href = `/editDestructable/${project.id}`;
         } else {
-            window.location.href = '/signup' // commented out for debugging
+            window.location.href = '/signup'
         };
     });
 })
