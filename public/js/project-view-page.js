723eeb0ee5b4cfbc51e1292ecac62262421252ce
Krisna Sarith <krisnasarith@gmail.com>
2020-08-17 06:54:00 -0700

    filtered projects without categorys

--- public/js/project-view-page.js
+++ public/js/project-view-page.js
@@ -7,51 +7,36 @@ const projectStepsContainer = document.getElementById("project-steps-container")
 const editProjectButton = document.getElementById("edit");
 let project;
 
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function createCarousel() {
-    return `
-        <div class="container my-4">
-            <!--Carousel Wrapper-->
-            <div id="carousel-thumb" class="carousel slide carousel-fade carousel-thumbnails" data-ride="carousel">
-            <!--Slides-->
-            <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg" alt="Third slide">
-                </div>
+    let projectImages = project.images;
+    console.log(projectImages)
+    let projectImagesString = '';
+    if (projectImages) {
+        let randomImageIndex = getRndInteger(0, projectImages.length)
+        let projectImage = projectImages[randomImageIndex];
+        console.log(projectImage);
+        // let splitProjectImageString = projectImage.split('.');
+        // if (splitProjectImageString[randomImageIndex] = 'jpeg') {
+        //     splitProjectImageString[randomImageIndex] = 'jpg'
+        //     projectImage = splitProjectImageString.join('.')
+        // }
+        projectImagesString += `
+            <div class="container text-center my-4">
+                <img src="https://destructables-storage-dev.s3-us-west-1.amazonaws.com/${projectImage}">
             </div>
-            <!--/.Slides-->
-            <!--Controls-->
-            <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            <!--/.Controls-->
-            <ol class="carousel-indicators">
-                <li data-target="#carousel-thumb" data-slide-to="0" class="active"> <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(88).jpg"
-                    class="img-fluid"></li>
-                <li data-target="#carousel-thumb" data-slide-to="1"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(121).jpg"
-                    class="img-fluid"></li>
-                <li data-target="#carousel-thumb" data-slide-to="2"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(31).jpg"
-                    class="img-fluid"></li>
-            </ol>
-            </div>
-            <!--/.Carousel Wrapper-->
-        </div>
-  `
+      `
+    }
+
+    return projectImagesString
 }
 
 
 let currentPath = window.location.href;
-const digitPath = (function() {
+const digitPath = (function () {
     let charCount = 0;
 
     if (currentPath[currentPath.length - 1] === '/') {
