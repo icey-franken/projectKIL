80641331dec401d74ae403b15bca62b333aea582
kcsarith <krisnasarith@gmail.com>
2021-04-02 09:26:46 -0700

    dialect options and seeder storage added to database

--- config/database.js
+++ config/database.js
@@ -20,5 +20,13 @@ module.exports = {
     },
     production: {
         use_env_variable: 'DATABASE_URL',
+        dialect: 'postgres',
+        seederStorage: 'sequelize',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     },
 };
